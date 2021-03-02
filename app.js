8da9da68a30a833f905591c469d27b48d0ee8b4f
Almuker <73536819+Almuker@users.noreply.github.com>
2021-03-02 17:14:09 +0600

    Vue todolist

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 const App = {
     data() {
         return {
-            counter: 0,
-            title: 'Счётчик'
+            placeholderString: 'Введите название заметки',
+            title: 'Список заметок',
+            inputValue: '',
+            notes: ['Заметка 1']
+        }
+    },
+    methods: {
+        inputChangeHandler(event) {
+            this.inputValue = event.target.value
+        },
+        addNewNote() {
+            if (this.inputValue !== '') {
+                this.notes.push(this.inputValue)
+                this.inputValue = ''
+            }
+        },
+        toUpperCase(item) {
+            return item.toUpperCase()
+        },
+        removeNote(index) {
+            this.notes.splice(index, 1)
+        }
+    },
+    computed: {
+        doubleCountComputed() {
+            console.log('doubleCountComputed')
+            return this.notes.length * 2
+        }
+    },
+    watch: {
+        inputValue(value) {
+            if (value.length > 10) {
+                this.inputValue = ''
+            }
+            console.log('input value changed', value)
         }
     }
+    
 }
 
 Vue.createApp(App).mount('#app')
